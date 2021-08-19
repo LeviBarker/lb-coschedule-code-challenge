9aebcbe8d4f2021a8ac0f9c34a98af0bd82137a4
Levi Barker <levi.barker@outlook.com>
2021-08-19 13:08:42 -0700

    favicon and root route change

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 import './admin';
 import 'firebase-functions';
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 
 import { commentsRouter, searchRouter, userRatingRouter } from './routes';
@@ -12,10 +12,6 @@ app.use(cors());
 app.use('/search', searchRouter);
 app.use('/user-rating', userRatingRouter);
 app.use('/comments', commentsRouter);
-
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).send({ data: 'all good' });
-});
 
 // Export API for gateway through Firbase Cloud Functions (Serverless)
 exports.api = functions.https.onRequest(app);
